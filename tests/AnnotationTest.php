a867cda4f16c18f4e85d121498950e08f1c1a86c
Jonathan Bennett <jonathan@jbennett.me>
2014-10-06 00:25:40 -0400

    equality testing

--- tests/AnnotationTest.php
+++ tests/AnnotationTest.php
@@ -15,4 +15,45 @@ class AnnotationTest extends PHPUnit_Framework_TestCase {
     $this->assertCount(0, $annotation->attributes());
   }
 
+  function testAnnotationIsEqualToSelf() {
+    $annotation = new Annotation("foo", 1, 50);
+
+    $this->assertTrue($annotation->isEqual($annotation));
+  }
+
+  function testAnnotationWithDifferentTypesAreNotEqual() {
+    $annotation1 = new Annotation("foo", 1, 50);
+    $annotation2 = new Annotation("bar", 1, 50);
+
+    $this->assertFalse($annotation1->isEqual($annotation2));
+  }
+
+  function testAnnotationWithDifferentStartsAreNotEqual() {
+    $annotation1 = new Annotation("foo", 1, 50);
+    $annotation2 = new Annotation("foo", 10, 50);
+
+    $this->assertFalse($annotation1->isEqual($annotation2));
+  }
+
+  function testAnnotationWithDifferentLengthsAreNotEqual() {
+    $annotation1 = new Annotation("foo", 1, 40);
+    $annotation2 = new Annotation("foo", 1, 50);
+
+    $this->assertFalse($annotation1->isEqual($annotation2));
+  }
+
+  function testAnnotationWithDifferentAttributesAreNotEqual() {
+    $annotation1 = new Annotation("foo", 1, 50);
+    $annotation2 = new Annotation("foo", 1, 50, array('foo' => 'bar'));
+
+    $this->assertFalse($annotation1->isEqual($annotation2));
+  }
+
+  function testAnnotationWithSamePropertiesShouldBeEqual() {
+    $annotation1 = new Annotation("foo", 1, 40);
+    $annotation2 = new Annotation("foo", 1, 40);
+
+    $this->assertTrue($annotation1->isEqual($annotation2));
+  }
+
 }
--- tests/BlockTest.php
+++ tests/BlockTest.php
@@ -29,4 +29,44 @@ class BlockTest extends PHPUnit_Framework_TestCase {
     $this->assertCount(0, $block->children());
   }
 
+  function testSameBlockIsEqual() {
+    $block = new Block();
+    $this->assertTrue($block->isEqual($block));
+  }
+
+  function testBlocksWithDifferentTypesAreNotEqual() {
+    $block1 = new Block("foo");
+    $block2 = new Block("bar");
+
+    $this->assertFalse($block1->isEqual($block2));
+  }
+
+  function testBlocksWithDifferentTextAreNotEqual() {
+    $block1 = new Block("foo", "text1");
+    $block2 = new Block("foo", "text2");
+
+    $this->assertFalse($block1->isEqual($block2));
+  }
+
+  function testBlocksWithDifferentAttributesAreNotEqual() {
+    $block1 = new Block("foo", "text", array());
+    $block2 = new Block("foo", "text", array(1));
+
+    $this->assertFalse($block1->isEqual($block2));
+  }
+
+  function testBlocksWithDifferentAnnotationsAreNotEqual() {
+    $block1 = new Block("foo", "text", array(), array());
+    $block2 = new Block("foo", "text", array(), array(1));
+
+    $this->assertFalse($block1->isEqual($block2));
+  }
+
+  function testBlocksWithDifferentChildrenAreNotEqual() {
+    $block1 = new Block("foo", "text", array(), array(), array());
+    $block2 = new Block("foo", "text", array(), array(), array(1));
+
+    $this->assertFalse($block1->isEqual($block2));
+  }
+
 }
