5c962cfbd58bb586f7b1c7fdff177547248f932c
Jonathan Bennett <jonathan@jbennett.me>
2014-10-10 12:34:37 -0400

    Remove functionality tests moved into ParserCollection

--- tests/HtmlParser/AnnotationFinderTest.php
+++ tests/HtmlParser/AnnotationFinderTest.php
@@ -1,44 +1,55 @@
 <?php
 require 'vendor/autoload.php';
+use StructuredText\Annotation;
 use StructuredText\HtmlParser\AnnotationFinder;
+use StructuredText\HtmlParser\ParserCollection;
 use StructuredText\HtmlParser\Annotations\BoldAnnotationParser;
 
-class AnnotationFinderTest { //extends PHPUnit_Framework_TestCase {
+class AnnotationFinderTest extends PHPUnit_Framework_TestCase {
 
   function testInit() {
     $finder = new AnnotationFinder();
     $this->assertNotNull($finder);
   }
 
-  function testRegistration() {
-    $finder = new AnnotationFinder();
-    $boldParser = new BoldAnnotationParser();
-    $finder->register($boldParser);
-
-    $this->assertCount(1, $finder->handlers());
-  }
-
   function testFindingOne() {
     $node = $this->getNodeForHTML('<p><b>hello</b></p>', 'p');
+    $collectionMock = $this->getMock(ParserCollection::class);
+    $collectionMock->method('getParserForNode')
+      ->will($this->returnValue(new BoldAnnotationParser()));
+    $annotion = new Annotation('.b', 0, 5);
+
     $finder = new AnnotationFinder();
-    $boldParser = new BoldAnnotationParser();
-    $finder->register($boldParser);
-    $annotations = $finder->findAnnotations($node);
+    $annotations = $finder->findMatches($node, $collectionMock);
 
     $this->assertCount(1, $annotations);
-    $this->assertEquals('.b', $annotations[0]->type());
+    $this->assertTrue($annotion->isEqual($annotations[0]));
   }
 
   function testFindingAtEnd() {
     $node = $this->getNodeForHTML('<p>hello <b>world</b></p>', 'p');
+    $collectionMock = $this->getMock(ParserCollection::class);
+    $collectionMock->method('getParserForNode')
+      ->will($this->returnValue(new BoldAnnotationParser()));
+
     $finder = new AnnotationFinder();
-    $boldParser = new BoldAnnotationParser();
-    $finder->register($boldParser);
-    $annotations = $finder->findAnnotations($node);
-    $expected = new \StructuredText\Annotation('.b', 6, 5);
+    $annotations = $finder->findMatches($node, $collectionMock);
+    $expected = new Annotation('.b', 6, 5);
 
     $this->assertCount(1, $annotations);
     $this->assertTrue($expected->isEqual($annotations[0]));
+  }
+
+  function testFindingMultiple() {
+    $node = $this->getNodeForHTML('<p><b>Hello</b> <b>World</b></p>', 'p');
+    $collectionMock = $this->getMock(ParserCollection::class);
+    $collectionMock->method('getParserForNode')
+      ->will($this->returnValue(new BoldAnnotationParser()));
+
+    $finder = new AnnotationFinder();
+    $annotations = $finder->findMatches($node, $collectionMock);
+
+    $this->assertCount(2, $annotations);
   }
 
   function getNodeForHTML($html, $tag) {
