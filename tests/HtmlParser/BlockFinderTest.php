59340d78a477e01d304a4e1c19a879e0d15c275b
Jonathan Bennett <jonathan@jbennett.me>
2014-10-20 14:14:38 -0400

    more block parser tests

--- tests/HtmlParser/BlockFinderTest.php
+++ tests/HtmlParser/BlockFinderTest.php
@@ -3,12 +3,14 @@ require 'vendor/autoload.php';
 use StructuredText\Block;
 use StructuredText\HtmlParser\BlockFinder;
 use StructuredText\HtmlParser\ParserCollection;
+use StructuredText\HtmlParser\Blocks\GreedyBlockParser;
 use StructuredText\HtmlParser\Blocks\ParagraphBlockParser;
 
 class BlockFinderTest extends PHPUnit_Framework_TestCase {
 
   function testInit() {
-    $finder = new BlockFinder();
+    $collectionMock = $this->getMock(ParserCollection::class);
+    $finder = new BlockFinder($collectionMock);
     $this->assertNotNull($finder);
   }
 
@@ -19,11 +21,26 @@ class BlockFinderTest extends PHPUnit_Framework_TestCase {
       ->will($this->returnValue(new ParagraphBlockParser()));
     $block = new Block('.paragraph', 'Hello');
 
-    $finder = new BlockFinder();
-    $blocks = $finder->findMatches($node, $collectionMock);
+    $finder = new BlockFinder($collectionMock);
+    $result = $finder->findMatches($node);
 
-    $this->assertCount(1, $blocks);
-    $this->assertTrue($block->isEqual($blocks[0]));
+    $this->assertTrue($block->isEqual($result));
+  }
+
+  function testFindingNested() {
+    $node = $this->getNodeForHTML('<div><blockquote>Hello World</blockquote></div>', 'div');
+    $collectionMock = $this->getMock(ParserCollection::class);
+    $collectionMock->method('getParserForNode')
+      ->will($this->returnValue(new GreedyBlockParser()));
+    $block = new Block('#blockquote', 'Hello World');
+
+    $finder = new BlockFinder($collectionMock);
+    $result = $finder->findMatches($node);
+
+    $this->assertEquals('#div', $result->type());
+    $this->assertEquals('Hello World', $result->text());
+    $this->assertCount(1, $result->children());
+    $this->assertTrue($block->isEqual(current($result->children())));
   }
 
 
