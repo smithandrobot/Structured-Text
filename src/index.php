16ed16e70d594f1410305a94ae98d4cec8da8709
Jonathan Bennett <jonathan@jbennett.me>
2014-10-21 09:42:20 -0400

    simple test of json renderer

--- src/index.php
+++ src/index.php
@@ -2,10 +2,13 @@
 
 require_once dirname(__FILE__) .'/../vendor/autoload.php';
 use StructuredText\HtmlParser\Parser;
+use StructuredText\JsonRender\JsonRenderer;
+
+
 
 $html = file_get_contents(dirname(__FILE__) . '/../tests/resources/complicated.html');
-
 $parser = Parser::configuredParser();
 $document = $parser->parse($html);
 
-print_r($document);
+StructuredText\JsonRender\JsonRenderer::render($document);
+//print_r($document);
